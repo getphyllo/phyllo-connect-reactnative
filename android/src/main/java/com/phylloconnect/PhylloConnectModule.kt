6f7515597998f72d13f13836b3e4a9078676971a
Sumukha Hegde <sumukha214@gmail.com>
2022-02-25 16:46:25 +0530

    android initialization part

--- android/src/main/java/com/phylloconnect/PhylloConnectModule.kt
+++ android/src/main/java/com/phylloconnect/PhylloConnectModule.kt
@@ -1,11 +1,25 @@
 package com.phylloconnect
-import com.getphyllo.*
 
-import android.util.Log
+import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-
+import java.util.Map
+import java.util.HashMap
+import android.util.Log
+import com.getphyllo.*
+import android.os.Looper
+import android.os.Handler
+import java.lang.Runnable
+import com.facebook.react.bridge.UiThreadUtil
+import android.view.WindowManager
+import android.view.Window
+import android.view.View
+import android.content.Intent
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class PhylloConnectModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -16,18 +30,17 @@ class PhylloConnectModule(reactContext: ReactApplicationContext) : ReactContextB
     override fun getConstants(): MutableMap<String, Any> {
         return hashMapOf("count" to 1)
     }
-    
-    @ReactMethod
-    public fun initialize(name: String, userId: String, token: String, platformId: String? = "") {
 
-        PhylloConnect.initialize(context = reactApplicationContext,
+    @ReactMethod
+    public fun initialize(name: String, token: String, userId: String, environment: String, platformId: String) {       
+        Handler(Looper.getMainLooper()).post {
+            PhylloConnect.initialize(context = reactApplicationContext,
             name = name,
             userId = userId,
             token = token,
             platformId = platformId,
             environment = PhylloConnect.ENVIRONMENT.DEVELOPMENT,
             callback = object : ConnectCallback {
-
                 override fun onAccountConnected(accountId: String?,platformId: String?, userId: String?) {
                     Log.d(logTag, "onAccountConnected $accountId $platformId  $userId")
                 }
@@ -52,8 +65,8 @@ class PhylloConnectModule(reactContext: ReactApplicationContext) : ReactContextB
                     Log.d(logTag, "onExit $userId $reason")
                 }
             })
-
-        PhylloConnect.open()
+            PhylloConnect.open()
+        }
     }
 
 }
