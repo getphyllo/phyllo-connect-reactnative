261bb67d4df141fa9f72db55e6e7d9fda9cb7611
mohit <mohit@getphyllo.com>
2022-02-25 15:21:20 +0530

    android native fixes

--- android/src/main/java/com/phylloconnect/PhylloConnectModule.kt
+++ android/src/main/java/com/phylloconnect/PhylloConnectModule.kt
@@ -1,5 +1,7 @@
-import com.getphyllo
+package com.phylloconnect
+import com.getphyllo.*
 
+import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 
@@ -7,20 +9,48 @@ class PhylloConnectModule(reactContext: ReactApplicationContext) : ReactContextB
 
     override fun getName() = "PhylloConnectModule"
 
+    val logTag: String = "PhylloConnectModule"
+
     override fun getConstants(): MutableMap<String, Any> {
         return hashMapOf("count" to 1)
     }
 
     public fun initialize(name: String, userId: String, token: String, platformId: String? = "") {
 
-        getphyllo.PhylloConnect.initialize(context = this@MainActivity,
+        PhylloConnect.initialize(context = reactApplicationContext,
             name = name,
             userId = userId,
             token = token,
             platformId = platformId,
-            environment = environment
-            )
+            environment = PhylloConnect.ENVIRONMENT.DEVELOPMENT,
+            callback = object : ConnectCallback {
+
+                override fun onAccountConnected(accountId: String?,platformId: String?, userId: String?) {
+                    Log.d(logTag, "onAccountConnected $accountId $platformId  $userId")
+                }
+
+                override fun onAccountDisconnected(accountId: String?,platformId: String?, userId: String?) {
+                    Log.d(logTag, "onAccountDisconnected $accountId $platformId  $userId")
+                }
+
+                override fun onError(errorMsg: String?) {
+                    Log.d(logTag, "on Error  errorMsg")
+                }
+
+                override fun onTokenExpired(userId: String?) {
+                    Log.d(logTag, "onTokenExpired  $userId")
+                }
+
+                override fun onEvent(event: PhylloConnect.EVENT) {
+                    Log.d(logTag, "onEvent  $event")
+                }
+
+                override fun onExit(reason:String?,userId: String?) {
+                    Log.d(logTag, "onExit $userId $reason")
+                }
+            })
 
         PhylloConnect.open()
     }
+
 }
