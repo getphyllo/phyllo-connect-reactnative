922f9d41cb481658ecf71a3321037edbe0ffe889
Sumukha Hegde <sumukha214@gmail.com>
2022-02-26 20:34:00 +0530

    remove phyllo credentials

--- example/src/config.js
+++ example/src/config.js
@@ -1,8 +1,8 @@
 import { PhylloEnvironment } from 'phyllo-connect-react-native'
 
 const config = {
-  clientId: '2778c997-f4bc-401d-95c4-6f2178b85404',
-  clientSecret: '535b916c-292b-4771-bd6a-41dc4a53db6f',
+  clientId: '',
+  clientSecret: '',
   env: PhylloEnvironment.Development,
 }
 
