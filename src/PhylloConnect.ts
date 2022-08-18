611b449f4d58a3f7e124b77d42edbe05c5f476d4
sumukha <97936731+sumukhaPhyllo@users.noreply.github.com>
2022-08-18 16:32:57 +0530

    Added optional callback

--- src/PhylloConnect.ts
+++ src/PhylloConnect.ts
@@ -5,6 +5,7 @@ import {
   PHYLLO_ON_EXIT_KEY,
   PHYLLO_ON_TOKEN_EXPIRED_KEY,
   callBacksDefintionArr,
+  callBacksDefinitionArr
 } from './constants'
 import { PhylloEnvironment } from './PhylloEnvironment'
 
@@ -41,25 +42,45 @@ const validateConfig = (params: IPhylloInitialize) => {
 }
 
 const validateCallbacks = (callbacksObj: any) => {
-  const keysArr = Object.keys(callbacksObj)
-  for (var i = 0; i < keysArr.length; i++) {
+  const keysArr = Object.keys(callbacksObj);
+  for(var i = 0 ; i < keysArr.length; i++) {
     // cheking if callbacks are passed by developer
-    if (!callbacksObj[keysArr[i]])
-      throw new Error('Please add the callback: ' + keysArr[i])
+    if(!callbacksObj[keysArr[i]]) {
+      // checking if the missing callback is an optional or mandatory callback and throw error accordingly
+      if(callBacksDefinitionArr.mandatoryCallbacks.filter(item => item.callbackName === keysArr[i]).length > 0) throw new Error("Please add the callback: "+ keysArr[i]);
+    }
 
     //checking if the required number of parameters are passed in the callback
-    if (
-      callbacksObj[keysArr[i]].length !==
-      callBacksDefintionArr.filter((key) => key.callbackName === keysArr[i])[0]
-        .argsLength
-    ) {
-      throw new Error(
-        'Please match the required number of parameters in callback: ' +
-          keysArr[i]
-      )
+    if(callbacksObj[keysArr[i]]) {
+      if(callbacksObj[keysArr[i]].length < [...callBacksDefinitionArr.mandatoryCallbacks,...callBacksDefinitionArr.optionalCallbacks].filter(key => key.callbackName === keysArr[i])[0].argsLength)  {
+        throw new Error("Please add the required number of parameters in callback: "+ keysArr[i]);
+    }
     }
   }
-}
+};
+
+// const validateCallbacks = (callbacksObj: any) => {
+//   const keysArr = Object.keys(callbacksObj)
+//   // ['connect', 'disconnect', 'tokenExpired']
+//   for (var i = 0; i < keysArr.length; i++) {
+//     // cheking if callbacks are passed by developer
+//     if (!callbacksObj[keysArr[i]])
+//       throw new Error('Please add the callback: ' + keysArr[i])
+    
+
+//     //checking if the required number of parameters are passed in the callback
+//     if (
+//       callbacksObj[keysArr[i]].length !==
+//       callBacksDefintionArr.filter((key) => key.callbackName === keysArr[i])[0]
+//         .argsLength
+//     ) {
+//       throw new Error(
+//         'Please match the required number of parameters in callback: ' +
+//           keysArr[i]
+//       )
+//     }
+//   }
+// }
 
 const attachCallbacks = (callbackObj: any) => {
   for (let key in callbackObj) {
@@ -77,12 +98,15 @@ const attachCallbacks = (callbackObj: any) => {
 }
 
 const PhylloConnectSDK = {
-  callbacksObj: {
-    [PHYLLO_ACCOUNT_CONNECTED_KEY.callbackName]: null,
-    [PHYLLO_ACCOUNT_DISCONNECTED_KEY.callbackName]: null,
-    [PHYLLO_ON_TOKEN_EXPIRED_KEY.callbackName]: null,
-    [PHYLLO_ON_EXIT_KEY.callbackName]: null,
-  },
+  callbacksObj: [...callBacksDefinitionArr.mandatoryCallbacks,...callBacksDefinitionArr.optionalCallbacks].forEach(key => {
+    callbacksObj[key.callbackName] = null
+  }),
+  // callbacksObj: {
+  //   [PHYLLO_ACCOUNT_CONNECTED_KEY.callbackName]: null,
+  //   [PHYLLO_ACCOUNT_DISCONNECTED_KEY.callbackName]: null,
+  //   [PHYLLO_ON_TOKEN_EXPIRED_KEY.callbackName]: null,
+  //   [PHYLLO_ON_EXIT_KEY.callbackName]: null,
+  // },
   initialize: function ({
     clientDisplayName,
     token,
@@ -119,6 +143,7 @@ const PhylloConnectSDK = {
     }
   },
 }
+
 
 // export the object
 export default PhylloConnectSDK
