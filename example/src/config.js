f75121cc3ea6d7b05e89ced783d2ab20f4f2580c
Pankaj Patel <50038197+erpankajpatel@users.noreply.github.com>
2022-09-02 14:18:00 +0530

    Config update

--- example/src/config.js
+++ example/src/config.js
@@ -1,6 +1,6 @@
-const clientId = 'de5a5733-5d95-407f-a926-a94a6fb6c22c' // add your Id here
-const clientSecret = '5d4e0b3e-50db-4bb0-986d-b054c8b41359' // add your client Secret
-const env = 'production' // add your environment type, development, sandbox, production are the valid values
+const clientId = '' // add your Id here
+const clientSecret = '' // add your client Secret
+const env = 'sandbox' // add your environment type, development, sandbox, production are the valid values
 
 export default {
   clientId,
