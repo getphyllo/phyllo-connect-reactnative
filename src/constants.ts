be968c694a2a43ac4a964cffae2830661a885131
Chirag Arora <chirag@getphyllo.com>
2022-09-02 18:56:39 +0530

    added dynamic callbacksObj

--- src/constants.ts
+++ src/constants.ts
@@ -1,42 +1,50 @@
-export const PHYLLO_ACCOUNT_CONNECTED_KEY = {
+export interface ICallbacks {
+  callbackName: string
+  argsArr: Array<string>
+  argsLength: number
+}
+
+export const PHYLLO_ACCOUNT_CONNECTED_KEY: ICallbacks = {
   callbackName: 'accountConnected',
   argsArr: ['accountId', 'workplatformId', 'userId'],
   argsLength: 3,
 }
 
-export const PHYLLO_ACCOUNT_DISCONNECTED_KEY = {
+export const PHYLLO_ACCOUNT_DISCONNECTED_KEY: ICallbacks = {
   callbackName: 'accountDisconnected',
   argsArr: ['accountId', 'workplatformId', 'userId'],
   argsLength: 3,
 }
 
-export const PHYLLO_ON_TOKEN_EXPIRED_KEY = {
+export const PHYLLO_ON_TOKEN_EXPIRED_KEY: ICallbacks = {
   callbackName: 'tokenExpired',
   argsArr: ['userId'],
   argsLength: 1,
 }
 
-export const PHYLLO_ON_EXIT_KEY = {
+export const PHYLLO_ON_EXIT_KEY: ICallbacks = {
   callbackName: 'exit',
   argsArr: ['reason', 'userId'],
   argsLength: 2,
 }
 
-
-export const PHYLLO_ON_CONNECTION_FAILURE_KEY = {
-  callbackName : 'connectionFailure',
-  argsArr : ['reason','workplatformId', 'userId'],
-  argsLength : 3
+export const PHYLLO_ON_CONNECTION_FAILURE_KEY: ICallbacks = {
+  callbackName: 'connectionFailure',
+  argsArr: ['reason', 'workplatformId', 'userId'],
+  argsLength: 3,
 }
 
-export const callBacksDefinitionArr = {
-  mandatoryCallbacks : new Array(
+interface ICallbackDefinitions {
+  mandatoryCallbacks: Array<ICallbacks>
+  optionalCallbacks: Array<ICallbacks>
+}
+
+export const callBacksDefinitionArr: ICallbackDefinitions = {
+  mandatoryCallbacks: new Array(
     PHYLLO_ACCOUNT_CONNECTED_KEY,
     PHYLLO_ACCOUNT_DISCONNECTED_KEY,
     PHYLLO_ON_TOKEN_EXPIRED_KEY,
     PHYLLO_ON_EXIT_KEY
   ),
-  optionalCallbacks : new Array(
-    PHYLLO_ON_CONNECTION_FAILURE_KEY
-  )
+  optionalCallbacks: new Array(PHYLLO_ON_CONNECTION_FAILURE_KEY),
 }
