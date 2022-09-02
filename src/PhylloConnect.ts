9603f76492ae50c3dc0060d60cd6595245abbbb6
Chirag Arora <95206257+chiragphyllo@users.noreply.github.com>
2022-09-02 16:39:05 +0530

    Entered dynamic callback definition for new callbacks

--- src/PhylloConnect.ts
+++ src/PhylloConnect.ts
@@ -1,47 +1,47 @@
-import { NativeModules, NativeEventEmitter } from 'react-native'
+import { NativeModules, NativeEventEmitter } from "react-native";
 import {
   PHYLLO_ACCOUNT_CONNECTED_KEY,
   PHYLLO_ACCOUNT_DISCONNECTED_KEY,
   PHYLLO_ON_EXIT_KEY,
   PHYLLO_ON_TOKEN_EXPIRED_KEY,
   callBacksDefinitionArr,
-} from './constants'
-import { PhylloEnvironment } from './PhylloEnvironment'
+} from "./constants";
+import { PhylloEnvironment } from "./PhylloEnvironment";
 
 interface IPhylloInitialize {
-  clientDisplayName: string
-  token: string
-  userId: string
-  environment: PhylloEnvironment
-  workPlatformId?: string
+  clientDisplayName: string;
+  token: string;
+  userId: string;
+  environment: PhylloEnvironment;
+  workPlatformId?: string;
 }
 
 type TEventType =
-  | 'accountConnected'
-  | 'accountDisconnected'
-  | 'exit'
-  | 'tokenExpired'
+  | "accountConnected"
+  | "accountDisconnected"
+  | "exit"
+  | "tokenExpired";
 
-const phyllo = NativeModules.PhylloConnectModule
-const eventEmitter = new NativeEventEmitter(phyllo)
+const phyllo = NativeModules.PhylloConnectModule;
+const eventEmitter = new NativeEventEmitter(phyllo);
 
 const validateConfig = (params: IPhylloInitialize) => {
   if (!params.environment || !(params.environment in PhylloEnvironment)) {
-    throw new Error('Please provide a valid environment')
+    throw new Error("Please provide a valid environment");
   }
   if (!params.userId) {
-    throw new Error('Please provide a User Id')
+    throw new Error("Please provide a User Id");
   }
   if (!params.clientDisplayName) {
-    throw new Error('Please provide Client Display Name')
+    throw new Error("Please provide Client Display Name");
   }
   if (!params.token) {
-    throw new Error('Please provide a Token')
+    throw new Error("Please provide a Token");
   }
-}
+};
 
 const validateCallbacks = (callbacksObj: any) => {
-  const keysArr = Object.keys(callbacksObj)
+  const keysArr = Object.keys(callbacksObj);
   for (var i = 0; i < keysArr.length; i++) {
     // cheking if callbacks are passed by developer
     if (!callbacksObj[keysArr[i]]) {
@@ -51,7 +51,7 @@ const validateCallbacks = (callbacksObj: any) => {
           (item) => item.callbackName === keysArr[i]
         ).length > 0
       )
-        throw new Error('Please add the callback: ' + keysArr[i])
+        throw new Error("Please add the callback: " + keysArr[i]);
     }
 
     //checking if the required number of parameters are passed in the callback
@@ -64,13 +64,13 @@ const validateCallbacks = (callbacksObj: any) => {
         ].filter((key) => key.callbackName === keysArr[i])[0].argsLength
       ) {
         throw new Error(
-          'Please add the required number of parameters in callback: ' +
+          "Please add the required number of parameters in callback: " +
             keysArr[i]
-        )
+        );
       }
     }
   }
-}
+};
 
 // const validateCallbacks = (callbacksObj: any) => {
 //   const keysArr = Object.keys(callbacksObj)
@@ -96,45 +96,46 @@ const validateCallbacks = (callbacksObj: any) => {
 
 const attachCallbacks = (callbackObj: any) => {
   for (let key in callbackObj) {
-    const eventName = 'on' + key[0].toUpperCase() + key.slice(1)
-    eventEmitter.removeAllListeners(eventName)
+    const eventName = "on" + key[0].toUpperCase() + key.slice(1);
+    eventEmitter.removeAllListeners(eventName);
   }
 
   for (let key in callbackObj) {
-    const eventName = 'on' + key[0].toUpperCase() + key.slice(1)
+    const eventName = "on" + key[0].toUpperCase() + key.slice(1);
     const sysCallback = (body: Array<string>) => {
-      callbackObj[key](...body)
-    }
-    eventEmitter.addListener(eventName, sysCallback)
+      callbackObj[key](...body);
+    };
+    eventEmitter.addListener(eventName, sysCallback);
   }
-}
+};
+const callbacksObj = [
+  ...callBacksDefinitionArr.mandatoryCallbacks,
+  ...callBacksDefinitionArr.optionalCallbacks,
+].forEach((key) => {
+  callbacksObj[key.callbackName] = null;
+});
 
 const PhylloConnectSDK = {
-  callbacksObj: {
-    [PHYLLO_ACCOUNT_CONNECTED_KEY.callbackName]: null,
-    [PHYLLO_ACCOUNT_DISCONNECTED_KEY.callbackName]: null,
-    [PHYLLO_ON_TOKEN_EXPIRED_KEY.callbackName]: null,
-    [PHYLLO_ON_EXIT_KEY.callbackName]: null,
-  },
   // callbacksObj: {
   //   [PHYLLO_ACCOUNT_CONNECTED_KEY.callbackName]: null,
   //   [PHYLLO_ACCOUNT_DISCONNECTED_KEY.callbackName]: null,
   //   [PHYLLO_ON_TOKEN_EXPIRED_KEY.callbackName]: null,
   //   [PHYLLO_ON_EXIT_KEY.callbackName]: null,
   // },
+  callbacksObj,
   initialize: function ({
     clientDisplayName,
     token,
     userId,
     environment,
-    workPlatformId = '',
+    workPlatformId = "",
   }: IPhylloInitialize) {
     validateConfig({
       clientDisplayName,
       token,
       userId,
       environment,
-    })
+    });
 
     // maintain the same order
     phyllo.initialize(
@@ -143,21 +144,21 @@ const PhylloConnectSDK = {
       userId,
       environment,
       workPlatformId
-    )
+    );
 
     // this is to solely match web sdk signature
     return {
       open: () => {
-        validateCallbacks(this.callbacksObj)
-        attachCallbacks(this.callbacksObj)
-        phyllo.open()
+        validateCallbacks(this.callbacksObj);
+        attachCallbacks(this.callbacksObj);
+        phyllo.open();
       },
       on: (event: string, callback: any) => {
-        this.callbacksObj[event] = callback
+        this.callbacksObj[event] = callback;
       },
-    }
+    };
   },
-}
+};
 
 // export the object
-export default PhylloConnectSDK
+export default PhylloConnectSDK;
